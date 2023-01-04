07023ce368e3d2c5d764f28255279d3135d7c735
Mads Tinko Madsen <lyderslev1@gmail.com>
2023-01-04 21:01:40 +0100

    Added login controller logic, and JWT Token generator logic.

---
+++ H4_Poker_Engine/Authentication/TokenGenerator.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace H4_Poker_Engine.Authentication
+{
+    public class TokenGenerator
+    {
+        #region fields
+        private readonly IConfiguration _configuration;
+        private Claim _username;
+        private Claim _role;
+        private DateTime _expires;
+        #endregion
+
+        #region Constructors
+        public TokenGenerator(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Sets the fields for the JWT token
+        /// Sets Name to Guest since it's only an authentication token.
+        /// Sets Gives the role "LoginRole" to the token.
+        /// And adds a live time of 10 minuetes for the token.
+        /// </summary>
+        /// <returns>A string based JWT token.</returns>
+        public string GenerateLoginToken()
+        {
+            _username = new Claim(ClaimTypes.Name, "Guest");
+            _role = new Claim(ClaimTypes.Role, "LoginRole");
+            _expires = DateTime.Now.AddMinutes(10);
+            return GenerateToken();
+        }
+
+        /// <summary>
+        /// Sets the fields for the JWT token.
+        /// Sets _name to the userinputted name from the app.
+        /// Sets _role "UserRole" for the token.
+        /// And adds a live time of 1 day for the token,
+        /// to not disconnect the user, in the middle of a game.
+        /// </summary>
+        /// <param name="username">The username the user has inputted in the app's login form.</param>
+        /// <returns>A string based JWT token.</returns>
+        public string GenerateUserToken(string username)
+        {
+            _username = new Claim(ClaimTypes.Name, username);
+            _role = new Claim(ClaimTypes.Role, "UserRole");
+            _expires = DateTime.Now.AddDays(1);
+            return GenerateToken();
+        }
+
+        /// <summary>
+        /// Generates a JWT token used for authentication.
+        /// Encrypted with a HMAC SHA512 algorithms.
+        /// </summary>
+        /// <returns>A string based JWT token.</returns>
+        private string GenerateToken()
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                _username,
+                _role
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            //Creates the token with the credentials, the key and the roles etc we have made earlier.
+            var token = new JwtSecurityToken(
+                claims: claims,
+                signingCredentials: credentials,
+                expires: _expires
+                );
+            //Converts the token to a string we can return.
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+        #endregion
+    }
+}
--- H4_Poker_Engine/Program.cs
+++ H4_Poker_Engine/Program.cs
@@ -1,11 +1,51 @@
+using H4_Poker_Engine.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// add auth header to swagger!
+// Makes us able to use Swagger documentation.
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the bearer scheme (\"Bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    // A filter added so swagger can figure out how to lock and unlock the tokens.
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+
+// this is adding the authentication schema to the application 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            // Get's the key from Appsettings, and uses the key to sign the tokens.
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        };
+    });
+
+// DependencyInjection
+builder.Services.AddScoped<TokenGenerator>();
 
 var app = builder.Build();
 
@@ -16,13 +56,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// TODO: Test HTTPS connection when HTTP is running correctly.
+//app.UseHttpsRedirection();
+
+// Added authentication, make sure it is instantiated above authorization
+// Won't authenticate if placed under UseAuthorization.
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
-
-
-//New test message
