8f396b3631b9207166421b2ae94867e6c07a00d6
WizzyVeigar <Seje_kenneth98@hotmail.com>
2023-01-04 15:50:12 +0100

    Started a player class and a base rule set

---
+++ H4_Poker_Engine/BaseRuleSet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using H4_Poker_Engine.Poco;
+
+namespace H4_Poker_Engine
+{
+    public abstract class BaseRuleSet
+    {
+        private int minimumPlayers;
+
+        public int MinimumPlayers
+        {
+            get { return minimumPlayers; }
+            private set { minimumPlayers = value; }
+        }
+
+        private int maximumPlayers;
+
+        public int MaximumPlayers
+        {
+            get { return maximumPlayers; }
+            protected set { maximumPlayers = value; }
+        }
+
+
+        public abstract void RunPokerGame();
+
+        protected virtual void AssignRoles()
+        {
+
+        }
+
+        protected abstract void DealCards();
+        protected abstract void BettingRound();
+        protected virtual Player DetermineWinner()
+        {
+
+        }
+    }
+}
---
+++ H4_Poker_Engine/Poco/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace H4_Poker_Engine.Poco
+{
+    //TODO give player turkey coins and a list of cards
+    public class Player
+    {
+        private string username;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        private bool active;
+
+        public bool Active
+        {
+            get { return active; }
+            set { active = value; }
+        }
+
+
+    }
+}
