fce29ba50f5e86259385cbed96c5363643d5c4a6
anit3k <steffen.halberg@gmail.com>
2023-01-06 09:48:47 +0100

    NOT WORKING - Need to refactor, remove secret keys from appsettings and add to environment insted

---
+++ H4_Poker_Engine.UnitTest/AuthenticationTest.cs
@@ -0,0 +1,33 @@
+﻿using H4_Poker_Engine.Authentication;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H4_Poker_Engine.UnitTest
+{
+    public class AuthenticationTest
+    {
+        //private TokenGenerator _tokenGenerator;
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void GuestTokenShouldWork()
+        {
+            var mock = new Mock<TokenGenerator>();
+            string loginToken = string.Empty;
+            mock.Setup( x => x.GenerateLoginToken());
+
+            Assert.That(loginToken, Is.Not.Null);
+            Assert.That(loginToken, Is.Not.EqualTo(""));
+        }
+    }
+}
---
+++ H4_Poker_Engine.UnitTest/LoginTest.cs
@@ -0,0 +1,35 @@
+﻿using H4_Poker_Engine.Authentication;
+using H4_Poker_Engine.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H4_Poker_Engine.UnitTest
+{
+    internal class LoginTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Arrange
+            var controller = new LoginController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            var response = controller.Get();
+
+            // Assert
+            Product product;
+            Assert.IsTrue(response.TryGetContentValue<Product>(out product));
+            Assert.AreEqual(10, product.Id);
+        }
+    }
+}
