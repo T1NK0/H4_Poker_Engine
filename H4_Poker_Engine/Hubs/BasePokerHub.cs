3c385bd04e868ac6633cf4b65366349cc1e43526
anit3k <steffen.halberg@gmail.com>
2023-01-08 16:17:28 +0100

    Now testet with [Authorize] added to hub

--- H4_Poker_Engine/Hubs/BasePokerHub.cs
+++ H4_Poker_Engine/Hubs/BasePokerHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace H4_Poker_Engine.Hubs
 {
@@ -8,16 +9,19 @@ namespace H4_Poker_Engine.Hubs
         public event Action<string, string, string>? PlayerHasDisconnectedEvent;
         public event Action<string, string, string>? PlayerIsReadyEvent;
 
+        [Authorize]
         public async Task PlayerConnected(string user, string message, string clientId)
         {
             NewPlayerConnectedEvent?.Invoke(user, message, clientId);
         }
 
+        [Authorize]
         public async Task PlayerDisconnected(string user, string message, string clientId)
         {
             PlayerHasDisconnectedEvent?.Invoke(user, message, clientId);
         }
 
+        [Authorize]
         public async Task PlayerIsReady(string user, string message, string clientId)
         {
             PlayerIsReadyEvent?.Invoke(user, message, clientId);
