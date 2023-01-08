f39d74db62fcd16ea6acf07b64dd90a87de8edff
anit3k <steffen.halberg@gmail.com>
2023-01-08 16:13:31 +0100

    2 players can now connect and get cards when ready

---
+++ H4_Poker_Engine/Models/CardModel.cs
@@ -0,0 +1,14 @@
+﻿namespace H4_Poker_Engine.Models
+{
+    public class CardModel
+    {
+        public string Suit { get; set; }
+        public string Value { get; set; }
+
+        public CardModel(string suit, string value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+    }
+}
--- H4_Poker_Engine/Services/TableServiceWorker.cs
+++ H4_Poker_Engine/Services/TableServiceWorker.cs
@@ -1,6 +1,8 @@
 ﻿using H4_Poker_Engine.Hubs;
 using H4_Poker_Engine.Models;
 using Microsoft.AspNetCore.SignalR;
+using System.Numerics;
+using System.Security.Cryptography;
 
 namespace H4_Poker_Engine.Services
 {
@@ -8,6 +10,8 @@ namespace H4_Poker_Engine.Services
     {
         private readonly IHubContext<BasePokerHub> _hubContext;
         private List<Player> _users;
+        private List<CardModel> _deck;
+        private bool _isGameRunning = false;
 
         public TableServiceWorker(IHubContext<BasePokerHub> hubContext, BasePokerHub hub)
         {
@@ -49,6 +53,88 @@ namespace H4_Poker_Engine.Services
                 playerToBeReady.Active = true;
                 Console.WriteLine($"Player {user} is ready to play, status is set to {playerToBeReady.Active}");
             }
+            CheckIfGameCanBegin();
+        }
+
+        private void CheckIfGameCanBegin()
+        {
+            var numberOfPlayersReady = 0;
+            foreach (var player in _users)
+            {
+                if (player.Active)
+                {
+                    numberOfPlayersReady++;
+                }
+            }
+            if (numberOfPlayersReady >= 2 && _isGameRunning == false)
+            {
+                _isGameRunning = true;
+                GiveCards();
+            }
+        }
+
+        private async void GiveCards()
+        {
+            GetANewDeck();
+            foreach (var player in _users)
+            {
+                var firstCard = _deck.FirstOrDefault();
+                _deck.Remove(firstCard);
+                var secondCard = _deck.FirstOrDefault();
+                _deck.Remove(secondCard);
+                await _hubContext.Clients.Client(player.ClientId).SendAsync("GetPlayerCards", firstCard, secondCard);
+            }
+            var tableFirstCard = _deck.FirstOrDefault();
+            _deck.Remove(tableFirstCard);
+            var tableSecondCard = _deck.FirstOrDefault();
+            _deck.Remove(tableSecondCard);
+            var tableTheedCard = _deck.FirstOrDefault();
+            _deck.Remove(tableTheedCard);
+            await _hubContext.Clients.All.SendAsync("GetTableCards", tableFirstCard, tableSecondCard, tableTheedCard);
+        }
+
+        private void GetANewDeck()
+        {
+            if (_deck == null)
+            {
+                _deck = new List<CardModel>();
+            }
+            else
+            {
+                _deck.Clear();
+            }
+            List<CardModel> deck = new List<CardModel>();
+
+            string[] suits = { "hearts", "diamonds", "spades", "clubs" };
+            string[] values = { "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
+
+            foreach (string suit in suits)
+            {
+                foreach (string value in values)
+                {
+                    deck.Add(new CardModel(suit, value));
+                }
+            }
+
+            _deck = Shuffle(deck);
+        }
+
+        private List<CardModel> Shuffle(List<CardModel> deck)
+        {
+            var rng = RandomNumberGenerator.Create();
+            int n = deck.Count;
+            while (n > 1)
+            {
+                byte[] box = new byte[1];
+                do rng.GetBytes(box);
+                while (!(box[0] < n * (Byte.MaxValue / n)));
+                int k = (box[0] % n);
+                n--;
+                CardModel value = deck[k];
+                deck[k] = deck[n];
+                deck[n] = value;
+            }
+            return deck;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
